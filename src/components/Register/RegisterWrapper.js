9fe9b77a845f6a49131f76110277535aeba05855
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-25 15:39:44 +0800

    Added validations for inputs

--- src/components/Register/RegisterWrapper.js
+++ src/components/Register/RegisterWrapper.js
@@ -11,12 +11,40 @@ export class RegisterWrapper extends Component {
     }
 
     componentDidMount() {
-        this.props.getAllUsers();
+        this.props.getAllUsers()
     }
     
 
     onSubmit = (e) => {
         e.preventDefault()
+
+        if (!new RegExp(".+@.+\\..+").test(this.state.email)) {
+            alert('Invalid Email')
+            return
+        }
+
+        if (!new RegExp("[0-9]{11}").test(this.state.phoneNumber)) {
+            alert('Invalid Number, must be 11-digit number')
+            return
+        }
+
+        if (!new RegExp("[a-zA-Z0-9]{5}").test(this.state.name)) {
+            alert('Invalid Name, must contain at least 5 characters')
+            return
+        }
+
+        if (!new RegExp("[a-zA-Z0-9]{3}").test(this.state.password)) {
+            alert('Invalid Name, must contain at least 3 characters')
+            return
+        }
+
+        if (this.state.password != this.state.confirmPassword) {
+            alert('Passwords do not match')
+            return
+        }
+
+        
+
         let newUser = {
             email: this.state.email,
             phoneNumber: this.state.phoneNumber,
@@ -24,11 +52,13 @@ export class RegisterWrapper extends Component {
             password: this.state.password,
             confirmPassword: this.state.confirmPassword
         }
+
+
         this.props.createUser(newUser)
     }
 
     onChangeEmail = (e) => {
-        this.setState({email: e.target.value})    
+        this.setState({email: e.target.value})
     }
     onChangePhoneNumber = (e) => {
         this.setState({phoneNumber: e.target.value})
@@ -46,13 +76,12 @@ export class RegisterWrapper extends Component {
     render() {
         return (
             <div className="register-wrapper">
-                {console.log(this.props.users)}
                 <form onSubmit = {this.onSubmit}>
                     <input onChange = {this.onChangeEmail} placeholder = "Email"/><br/>
                     <input onChange = {this.onChangePhoneNumber} placeholder = "Phone Number"/><br/>
                     <input onChange = {this.onChangeName} placeholder = "Name"/><br/>
-                    <input onChange = {this.onChangePassword} placeholder = "Password"/><br/>
-                    <input onChange = {this.onChangeConfirmPassword} placeholder = "Confirm Password"/><br/>
+                    <input type="password" onChange = {this.onChangePassword} placeholder = "Password"/><br/>
+                    <input type="password" onChange = {this.onChangeConfirmPassword} placeholder = "Confirm Password"/><br/>
                     <button type = "submit">Register</button>
                 </form>
             </div>
