606942f1b7f8ca6d2e7ebf7ae7419e1fffd81e96
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-28 15:22:53 +0800

    Add partially functioning autocomplete

--- src/api/LocationResource.js
+++ src/api/LocationResource.js
@@ -1,6 +1,10 @@
 export default {
+    getAll: () => fetch(
+        "http://localhost:8080/api/locations",
+        { mode: 'cors' }
+    ),
     findByName: (locationName) => fetch(
-        "http://localhost:8080/api/locations?name=" + locationName,
+        "http://localhost:8080/api/locations/search?name=" + locationName,
         { mode: 'cors' }
     )
 }
--- src/components/ParkingLotList/ParkingLotListInput.js
+++ src/components/ParkingLotList/ParkingLotListInput.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
-import { Select, TextInput } from 'react-materialize';
+import { Select, Autocomplete } from 'react-materialize';
+import LocationResource from '../../api/LocationResource';
 
 export default class ParkingLotListInput extends Component {
 
     state = {
         sortCriteria: 'Default',
-        manualLocation: 'None'
+        manualLocation: 'None',
+        locations: null
     };
+
+    componentDidMount() {
+        this.getLocations();
+    }
 
     handleInputChange = (event) => {
         this.setState({ sortCriteria: event.target.value });
@@ -18,11 +24,23 @@ export default class ParkingLotListInput extends Component {
         this.props.onSetManualLocation(event.target.value);
     }
 
+    getLocations = () => {
+        LocationResource.getAll()
+            .then(res => res.json())
+            .then(res => {
+                var mapped = res.map(item => ({ [item.name]: null }) );
+                var newObj = Object.assign({}, ...mapped );
+                this.setState({locations:newObj})
+            });
+    }
+
     render() {
+        if (!this.state.locations) {
+            return <div />
+        }
         return (
             <div className="todo-input">
-                <TextInput type="text" placeholder="Choose your location"
-                    onChange={this.onChangeManualLocation} />
+                <Autocomplete options={{data:this.state.locations}} placeholder="Choose your location" onChange={this.getLocations}/>
                 <Select value={this.state.sortCriteria} onChange={this.handleInputChange}>
                     <option value="Default" disabled>
                         Sort by...
