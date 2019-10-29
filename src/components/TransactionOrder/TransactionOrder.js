558a3999f7993c64bc2ecbaba4324531fd6a17b8
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-29 15:45:57 +0800

    Improve display of collapsible transaction orders

--- src/components/TransactionOrder/TransactionOrder.js
+++ src/components/TransactionOrder/TransactionOrder.js
@@ -1,53 +1,42 @@
 import React, { Component } from 'react';
 import { Row, Col, Collapsible, CollapsibleItem } from 'react-materialize';
 import StarRatings from 'react-star-ratings';
+import './TransactionOrder.css'
 
 export default class TransactionOrder extends Component {
     render() {
         const { transactionOrder } = this.props;
-        const transactionSummary = <div>{transactionOrder.parkingLotName}<br/><span style={{ fontSize: '11px' }}>Total: {transactionOrder.totalFee}</span></div>;
+        const transactionSummary = <div>{transactionOrder.parkingLotName}<br /><span style={{ fontSize: '11px' }}>Total: {transactionOrder.totalFee}</span></div>;
         return (
             <div>
-                    <Collapsible>
-                        <CollapsibleItem header={transactionSummary} >
-                            <span style={{ fontSize: '12px' }}> 
-                            Check-in: {transactionOrder.checkIn}<br/>
-                            Check-out: {transactionOrder.checkOut}<br/>
-                            Your rating: 
-                            <StarRatings
-                            rating={transactionOrder.starRating}
-                            starRatedColor="orange"
-                            numberOfStars={5}
-                            starDimension="12px"
-                            starSpacing="0.5px"
-                            name='rating'>
+                <Collapsible className="transaction-order-list">
+                    <CollapsibleItem header={transactionSummary} >
+                        <span style={{ fontSize: '12px' }}>
+                            <Row>
+                                <Col>Check-in:</Col>
+                                <Col>{transactionOrder.checkIn}</Col>
+                            </Row>                        
+                            <Row>
+                                <Col>Check-out:</Col>
+                                <Col>{transactionOrder.checkOut}</Col>
+                            </Row>                                                                                     
+                            <Row>
+                                <Col>Your rating:</Col>
+                                <Col>
+                                    <StarRatings
+                                        rating={transactionOrder.starRating}
+                                        starRatedColor="orange"
+                                        numberOfStars={5}
+                                        starDimension="12px"
+                                        starSpacing="0.5px"
+                                        name='rating'>
 
-                            </StarRatings>
-                            </span>
-                        </CollapsibleItem>
-                    </Collapsible>
-
-                        {/* {transactionOrder.parkingLotName}<br />
-
-                        <span style={{ fontSize: '11px' }}>Total: {transactionOrder.totalFee}</span> */}
-                        {/* {parkingLot.name}
-                        <br/>
-                        <span style={{fontSize: '11px'}}>{parseFloat(parkingLot.distance).toFixed(2)} km</span>
-                    </Col>
-                    <Col style={{float: 'right', fontSize: '11px'}}>
-                        ₱{parkingLot.rate}/hr
-                    </Col>
-                    <br/>
-                    <Col style={{float: 'right', fontSize: '11px'}}>
-                        <StarRatings
-                            rating={parkingLot.starRating}
-                            starRatedColor="orange"
-                            numberOfStars={5}
-                            starDimension="12px"
-                            starSpacing="0.5px"
-                            name='rating'>
-
-                        </StarRatings> */}
+                                    </StarRatings>
+                                </Col>
+                            </Row>
+                        </span>
+                    </CollapsibleItem>
+                </Collapsible>
             </div>
         )
     }
--- src/components/TransactionOrder/TransactionOrderList.js
+++ src/components/TransactionOrder/TransactionOrderList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Collection, CollectionItem, Row, Col} from 'react-materialize';
 import TransactionOrder from './TransactionOrder';
+import './TransactionOrder.css'
 
 export class TransactionOrderList extends Component {
 
@@ -12,7 +13,7 @@ export class TransactionOrderList extends Component {
                     <Col m={6} s={12}>
                         <Collection>
                             {transactionOrderList.map((transactionOrder, i) =>
-                                <CollectionItem href="#" key={i}>
+                                <CollectionItem href="#" key={i} className="transaction-order-list">
                                 <TransactionOrder transactionOrder={transactionOrder} />
                                 </CollectionItem>
                             )}
